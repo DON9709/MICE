d824c0f4beae327fc6c57af4bf3075f2415fc7b3
gksmf0160 <gksmf0160@gmail.com>
2025-08-29 20:26:21 +0900

    fix: BookmarkViewModel 카테고리 추가

--- MICE/ViewModel/BookmarkViewModel.swift
+++ MICE/ViewModel/BookmarkViewModel.swift
@@ -9,17 +9,17 @@ import Foundation
 
 class BookmarkViewModel {
     
-    // 저장한 스탬프 목록 (하드코딩된 데이터 3개)
     let savedStamps: [SearchResult] = [
-        SearchResult(imageName: "exhibition.dummy", placeName: "아침고요수목원 국화전시회", closedInfo: "매주 월요일 휴업", hoursInfo: "09:00 ~ 22:00", missionInfo: "진행중인 스탬프 미션: 1개"),
-        SearchResult(imageName: "exhibition.dummy", placeName: "경주 박물관", closedInfo: "매주 월요일 휴업", hoursInfo: "09:00 ~ 22:00", missionInfo: "진행중인 스탬프 미션: 1개"),
-        SearchResult(imageName: "exhibition.dummy", placeName: "부산 시립미술관", closedInfo: "매주 월요일 휴업", hoursInfo: "10:00 - 18:00", missionInfo: "진행중인 스탬프 미션: 1개")
+        // 각 항목에 category를 추가해주세요.
+        SearchResult(imageName: "exhibition.dummy", placeName: "아침고요수목원 국화전시회", category: "전시관", closedInfo: "매주 월요일 휴업", hoursInfo: "09:00 ~ 22:00", missionInfo: "진행중인 스탬프 미션: 1개"),
+        SearchResult(imageName: "exhibition.dummy", placeName: "경주 박물관", category: "박물관", closedInfo: "매주 월요일 휴업", hoursInfo: "09:00 ~ 22:00", missionInfo: "진행중인 스탬프 미션: 1개"),
+        SearchResult(imageName: "exhibition.dummy", placeName: "부산 시립미술관", category: "미술관", closedInfo: "매주 월요일 휴업", hoursInfo: "10:00 - 18:00", missionInfo: "진행중인 스탬프 미션: 1개")
     ]
     
-    // 다녀온 스탬프 목록 (하드코딩된 데이터 3개)
     let visitedStamps: [SearchResult] = [
-        SearchResult(imageName: "exhibition.dummy", placeName: "국립중앙박물관", closedInfo: "매주 월요일 휴업", hoursInfo: "10:00 - 18:00", missionInfo: "스탬프 미션 완료"),
-        SearchResult(imageName: "exhibition.dummy", placeName: "대림미술관", closedInfo: "매주 월요일 휴업", hoursInfo: "11:00 - 19:00", missionInfo: "스탬프 미션 완료"),
-        SearchResult(imageName: "exhibition.dummy", placeName: "지스타 2025", closedInfo: "행사 종료", hoursInfo: "10:00 - 18:00", missionInfo: "스탬프 미션 완료")
+        // 여기도 마찬가지로 category를 추가해주세요.
+        SearchResult(imageName: "exhibition.dummy", placeName: "국립중앙박물관", category: "박물관", closedInfo: "매주 월요일 휴업", hoursInfo: "10:00 - 18:00", missionInfo: "스탬프 미션 완료"),
+        SearchResult(imageName: "exhibition.dummy", placeName: "대림미술관", category: "미술관", closedInfo: "매주 월요일 휴업", hoursInfo: "11:00 - 19:00", missionInfo: "스탬프 미션 완료"),
+        SearchResult(imageName: "exhibition.dummy", placeName: "지스타 2025", category: "전시관", closedInfo: "행사 종료", hoursInfo: "10:00 - 18:00", missionInfo: "스탬프 미션 완료")
     ]
 }
