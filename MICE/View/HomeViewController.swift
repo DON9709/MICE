07cbb2bdfb788ea89764effb8de3b7c78d6da2b6
gksmf0160 <gksmf0160@gmail.com>
2025-09-08 13:27:05 +0900

    fix: 컬러 수정

--- MICE/View/HomeViewController.swift
+++ MICE/View/HomeViewController.swift
@@ -24,7 +24,7 @@ class HomeViewController: UIViewController {
         let label = UILabel()
         label.text = viewModel.mainTitle
         label.font = .systemFont(ofSize: 24, weight: .bold)
-        label.textColor = UIColor(red: 114/255.0, green: 76/25_5.0, blue: 249/255.0, alpha: 1)
+        label.textColor = UIColor(red: 114/255, green: 76/255, blue: 249/255, alpha: 1)
         return label
     }()
 
