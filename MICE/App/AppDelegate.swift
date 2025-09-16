3a5c9783e90772ba475f2911a57392df3777543e
DON <booby9709@gmail.com>
2025-09-16 13:49:26 +0900

    fix: 위치정보 권한 알림 기본alert만 뜨도록 변경

--- MICE/App/AppDelegate.swift
+++ MICE/App/AppDelegate.swift
@@ -16,7 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, CLLocationManagerDelegate
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // 앱 실행 후 초기 설정을 진행하는 지점
         locationManager.delegate = self
-        checkLocationPermissionIfNeeded()
+        locationManager.requestWhenInUseAuthorization()
         return true
     }
 
@@ -32,34 +32,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate, CLLocationManagerDelegate
         // 사용자가 씬 세션을 삭제했을 때 호출됨
         // 앱이 실행되지 않는 동안 삭제된 씬 세션도 이 메서드를 통해 정리됨
         // 삭제된 씬과 관련된 리소스를 해제하는 데 사용
-    }
-
-    private func checkLocationPermissionIfNeeded() {
-        let isFirstLaunch = !UserDefaults.standard.bool(forKey: "hasLaunchedBefore")
-
-        if isFirstLaunch {
-            UserDefaults.standard.set(true, forKey: "hasLaunchedBefore")
-            showLocationAlert()
-        }
-    }
-
-    private func showLocationAlert() {
-        let alert = UIAlertController(
-            title: "위치정보 사용 안내",
-            message: "MICE 앱은 위치 기반 추천 및 서비스를 위해 위치정보를 사용합니다. 위치 권한을 허용해 주세요.",
-            preferredStyle: .alert
-        )
-        alert.addAction(UIAlertAction(title: "허용", style: .default, handler: { _ in
-            self.locationManager.requestWhenInUseAuthorization()
-        }))
-        alert.addAction(UIAlertAction(title: "나중에", style: .cancel))
-
-        DispatchQueue.main.async {
-            if let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
-               let rootVC = windowScene.windows.first?.rootViewController {
-                rootVC.present(alert, animated: true)
-            }
-        }
     }
 
 }
